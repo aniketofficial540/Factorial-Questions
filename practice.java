32b9f414cb14f87c6cf5a6f54a147a4f789d78d3
aniketofficial540 <160538419+aniketofficial540@users.noreply.github.com>
2024-06-15 21:36:29 +0530

    initial commit

---
+++ practice.java
@@ -0,0 +1,58 @@
+import java.lang.*;
+import java.util.Scanner;
+
+/*
+public class practice {
+
+    //function for findng the product of two number
+    public static int product(int a, int b){
+        int c = a * b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the 1st number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter the 2nd number: ");
+        int num2 = sc.nextInt();
+
+        int result = product(num1, num2);
+
+        System.out.println("The product is: " + result);
+    }
+}
+*/
+
+/*
+public class practice {
+    public static int fact(int num){
+        int result = 1;
+        if (num == 0 ){
+            return 1;
+        }
+        else if (num == 1){
+            return 1;
+        }
+        else {
+            for (int i = 1; i <=num ; i++){
+                result = result * i;
+            }
+            return result;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        int result = fact(num);
+
+        System.out.println("The factorial of " + num + " is " + result);
+    }    
+}
+
+*/
