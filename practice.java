87fde932b3048f079c27394c6ede093d53e1ab72
aniketofficial540 <160538419+aniketofficial540@users.noreply.github.com>
2024-06-16 14:27:01 +0530

    Added new programmes
    Added to check the prime number and find the prime numbers in between the given range.

--- practice.java
+++ practice.java
@@ -2,6 +2,8 @@ import java.lang.*;
 import java.util.Scanner;
 
 /*
+
+finding product
 public class practice {
 
     //function for findng the product of two number
@@ -26,7 +28,10 @@ public class practice {
 }
 */
 
+
 /*
+
+//factorial
 public class practice {
     public static int fact(int num){
         int result = 1;
@@ -56,3 +61,70 @@ public class practice {
 }
 
 */
+
+/*
+//checking prime or not
+
+public class practice {
+    public static int prime(int num) {
+        if (num <= 1) {
+            return 0;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int num = sc.nextInt();
+         
+        int result = prime(num);
+
+        if (result == 1) {
+            System.out.println(num + " is a prime number");
+        } 
+        else {
+            System.out.println(num + " is not a prime number");
+        }
+    }
+}
+*/
+
+
+// prime in range
+public class practice {
+    public static int prime(int num) {
+        if (num <= 1) {
+            return 0;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the starting number: ");
+        int snum = sc.nextInt();
+         
+        System.out.println("Enter the end number: ");
+        int endnum = sc.nextInt();
+
+        for (int i = snum; i <= endnum; i++){
+            if (prime(i) == 1){
+                System.out.println(i + " is a prime number");
+            }
+        }
+    }
+}
+
